b0569ae591a1de477b4d8bcec690b42d5d73f89c
Anastasiia <guba.anastaysha@gmail.com>
2022-08-30 20:28:17 +0300

    task is done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+const galleryUl = document.querySelector('.gallery');
+
+const imagesMarkup = makeGalleryItemMarkup(galleryItems);
+galleryUl.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function makeGalleryItemMarkup(gallery) {
+    return gallery
+        .map(({ preview, original, description }) => {
+            return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+        })
+        .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+})
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlayerTimeupdate, 1000));
+
+player
+.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            console.log('The time was less than 0 or greater than the video`s duration')
+            break;
+
+        default:
+            console.log('Some other error occurred')
+            break;
+    }
+});
+
+function onPlayerTimeupdate(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+}
